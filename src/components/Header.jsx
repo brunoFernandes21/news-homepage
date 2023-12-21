cbeee3b2902c60bdb44bcfa7a918c4d7e7adc610
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-12-21 06:31:37 +0000

    implement mobile navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,24 @@
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+    const [showNav, setShowNav] = useState(false)
+
   return (
-    <div className="header_container">
+    <nav className={`header_container ${showNav ? "show__nav" : ""}`}>
       <img src="/src/assets/logo.svg" alt="news logo" />
-      <GiHamburgerMenu className="cursor-pointer text-4xl"/>
-      <ul className="header__links-desktop">
+      <GiHamburgerMenu onClick={() => setShowNav(true)} className="mobile__icon cursor-pointer text-4xl"/>
+      <AiOutlineClose onClick={() => setShowNav(false)} className="close__nav cursor-pointer text-4xl"/>
+
+      <ul className="header__links__desktop">
         <li>Home</li>
         <li>New</li>
         <li>Popular</li>
         <li>Trending</li>
         <li>Categories</li>
       </ul>
-    </div>
+    </nav>
   );
 };
 
