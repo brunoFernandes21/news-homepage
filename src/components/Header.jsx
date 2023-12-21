9561d4a35d9dd46ef5a7b8fb9ff973973b8ce12a
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-12-21 21:03:37 +0000

    finish header and hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
 
-const Header = () => {
-    const [showNav, setShowNav] = useState(false)
-
+const Header = ({ showNav, setShowNav }) => {
   return (
     <nav className={`header_container ${showNav ? "show__nav" : ""}`}>
       <img src="/src/assets/logo.svg" alt="news logo" />
-      <GiHamburgerMenu onClick={() => setShowNav(true)} className="mobile__icon cursor-pointer text-4xl"/>
-      <AiOutlineClose onClick={() => setShowNav(false)} className="close__nav cursor-pointer text-4xl"/>
+      <GiHamburgerMenu
+        onClick={() => setShowNav(true)}
+        className="mobile__icon cursor-pointer text-4xl"
+      />
+      <AiOutlineClose
+        onClick={() => setShowNav(false)}
+        className="close__nav cursor-pointer text-4xl"
+      />
 
       <ul className="header__links__desktop">
         <li>Home</li>
